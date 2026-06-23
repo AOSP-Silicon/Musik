e86aec0daa9637a2b334dc640782f9d930ced9cd
GhostMaster69-dev <rathore6375@gmail.com>
2026-06-23 16:32:45 +0530

    Build: Bump to v1.0.3
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,8 +101,8 @@ android {
         applicationId = applicationIdOverride ?: baseApplicationId
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
         resValue("string", "app_name", appNameOverride ?: "Musik")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
